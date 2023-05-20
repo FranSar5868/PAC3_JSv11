19f53e33d673521f688af2f1b2d4325ce543f8c1
fransar5868 <fsarrias@coit.es>
2023-05-20 23:41:46 +0200

    I add the js for managing thebuttons

--- script.js
+++ script.js
@@ -1,3 +1,79 @@
+const body = document.querySelector('#body');
+
+
+let themeColor = window.localStorage.getItem('tema');
+console.log(themeColor);
+if ( themeColor ) {
+  selectTheme(themeColor);
+  updateRadio(themeColor);
+} else {
+  selectTheme('system');
+  updateRadio('system');
+}
+
+function selectTheme(theme){
+  switch (theme) {
+    case 'dark':
+      console.log('aplicar dark');
+      body.classList.remove('light','system');
+      body.classList.add('dark');
+      window.localStorage.setItem('tema','dark');
+      break;
+    case 'light':
+      console.log('aplicar light');
+      body.classList.remove('dark','system');
+      body.classList.add('light');
+      window.localStorage.setItem('tema','light');
+      break;
+    case 'system':
+      console.log('aplicar system');
+      body.classList.remove('dark','light');
+      body.classList.add('system');
+      window.localStorage.setItem('tema','system');
+      break;
+  
+    default:
+      break;
+  }
+}
+
+function updateRadio( value ) {
+  switch (value) {
+    case 'dark':
+      console.log('dark');
+      document.getElementById('radioDark').checked = true;
+      document.getElementById('radioLight').checked = false;
+      document.getElementById('radioSystem').checked = false;
+      break;
+    case 'light':
+      console.log('light');
+      document.getElementById('radioLight').checked = true;
+      document.getElementById('radioDark').checked = false;
+      document.getElementById('radioSystem').checked = false;
+      break;
+    case 'system':
+      console.log('sytem');
+      document.getElementById('radioSystem').checked = true;
+      document.getElementById('radioLight').checked = false;
+      document.getElementById('radioDark').checked = false;
+      break;
+    default:
+      console.log('default');
+      document.getElementById('radioSystem').checked = true;
+      document.getElementById('radioLight').checked = false;
+      document.getElementById('radioDark').checked = false;
+      break;
+  }
+}
+
+const radios = document.querySelectorAll('input[name="theme"]');
+radios.forEach( x => {
+  x.addEventListener('change', function(){
+    selectTheme(this.value)
+  })
+});
+
+
 function fetchCards() {
     const maxNumofPokemons = 1010;
     const numCards = 10;
